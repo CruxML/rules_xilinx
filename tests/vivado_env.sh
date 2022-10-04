f1783bfcf8621965003ccd090b4a8642cf0492ff
PengBo-1024 <bo.peng@cruxml.com>
2022-10-04 14:22:46 +1100

    revert env path

--- tests/vivado_env.sh
+++ tests/vivado_env.sh
@@ -2,5 +2,5 @@
 
 # Vivido requires home to be set for the tcl store.
 export HOME=/tmp
-source /home/bopeng/legactVivado/Vivado/2020.1/settings64.sh
+source /opt/xilinx/Vivado/Vivado/2020.1/settings64.sh
 export XILINXD_LICENSE_FILE=2100@localhost
